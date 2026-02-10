6c1ddd829e84a15625e414992bd9455ae8e04cbb
Luis Sanchez <luis010993@gmail.com>
2026-02-10 16:52:03 +0100

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // AÑADE ESTO DE AQUÍ ABAJO 👇
+  preview: {
+    allowedHosts: ['impartial-spontaneity-production-36c6.up.railway.app'],
+  },
 })
